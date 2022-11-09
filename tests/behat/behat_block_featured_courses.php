09db89ed7242eeedf02c66ba3df08a17508d9cc0
martin-call-learning <113012859+martin-call-learning@users.noreply.github.com>
2022-11-09 15:26:24 +0100

    Fix behat (#5)
    * Added a patch so behat tests will be compatible with moodle 3 & 4
    * Fixed behat test & codechecker
    * Fixed camel cases in behat test

---
+++ tests/behat/behat_block_featured_courses.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing a class allowing set editing mode on either on moodle 3 & moodle 4.
+ *
+ * @package     block_featured_courses
+ * @copyright   2022 - CALL Learning
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Martin CORNU-MANSUY <martin@call-learning>
+ */
+
+/**
+ * A patch to make "I turn editing mode on" compatible with moodle 3.
+ *
+ * @package     block_featured_courses
+ * @copyright   2022 - CALL Learning
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Martin CORNU-MANSUY <martin@call-learning>
+ */
+class behat_block_featured_courses extends behat_base {
+
+    /**
+     * Turn on editing mode in moodle compatible with version 3 and 4.
+     *
+     * @Given I set editing mode on
+     */
+    public function i_set_editing_mode_on() {
+        global $CFG;
+        require_once($CFG->dirroot . "/lib/environmentlib.php");
+        $release = intval(get_config('', 'release')[0]);
+        if (normalize_version($release) > 3) {
+            $this->execute('behat_navigation::i_turn_editing_mode_on', []);
+        } else {
+            $this->execute('behat_general::i_click_on', ["Customise this page", "button"]);
+        }
+    }
+}
--- tests/behat/block_featured_courses.feature
+++ tests/behat/block_featured_courses.feature
@@ -11,34 +11,13 @@ Feature: Add a featured block on the dashboard
       | Course 4 | C4        | 0        | 1                |
 
   @javascript
-  Scenario: Add the block and set 3 featured courses
+  Scenario: Add the block, set 4 featured courses and remove one after, only two should display
     Given I log in as "admin"
-    Then I am on homepage
-    When I press "Customise this page"
-    And I add the "Featured Courses" block
+    And I set editing mode on
+    And I add the "Featured courses" block
     And I configure the "Featured courses" block
     And I expand all fieldsets
-    Then I should see "Title"
-    And I set the field "Title" to "My featured courses"
-    And I press "Add 3 more courses"
-    Then I set the following fields to these values:
-    | Selected course 1 | Course 1 |
-    | Selected course 2 | Course 2 |
-    | Selected course 3 | Course 3 |
-    And I set the field "Region" to "content"
-    And I press "Save changes"
-    Then I should see "Course 1" in the "My featured courses" "block"
-    Then I should see "Course 2" in the "My featured courses" "block"
-    Then I should see "Course 3" in the "My featured courses" "block"
-
-  @javascript
-  Scenario: Add the block and set 3 featured courses and remove one after, only two should display
-    Given I log in as "admin"
-    When I press "Customise this page"
-    Then I add the "Featured courses" block
-    And I configure the "Featured courses" block
-    And I expand all fieldsets
-    Then I should see "Title"
+    And I should see "Title"
     And I set the field "Title" to "My featured courses"
     And I press "Add 3 more courses"
     Then I set the following fields to these values:
